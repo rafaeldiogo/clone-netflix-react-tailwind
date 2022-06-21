58b62d3a84436350ef01358db5aa1a152952df72
rafaeldiogo <fael.diogo@hotmail.com>
2022-06-21 20:09:46 -0300

    react-tailwind

--- clone-netflix/src/components/Navbar.jsx
+++ clone-netflix/src/components/Navbar.jsx
@@ -1,13 +1,49 @@
+import { async } from "@firebase/util";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
-      <div>
-        <button className='text-white pr-4 '>Entrar</button>
-        <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Inscrever-se</button>
-      </div>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4 ">Conta</button>
+          </Link>
+            <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sair
+            </button>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4 ">Entrar</button>
+          </Link>
+          <Link to="signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Inscrever-se
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
