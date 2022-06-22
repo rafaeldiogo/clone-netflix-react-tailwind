ab0dafb0b3c329d139f441dc036cc1b9f242c052
rafaeldiogo <fael.diogo@hotmail.com>
2022-06-22 14:31:10 -0300

    react-tailwind

--- clone-netflix/src/pages/Home.jsx
+++ clone-netflix/src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div>
         <Main />
-        <Row rowID = '1' title = 'UpComing' fetchURL = {requests.requestUpcoming} />
-        <Row rowID = '2' title = 'Popular' fetchURL = {requests.requestPopular} />
-        <Row rowID = '3' title = 'Trending' fetchURL = {requests.requestTrending} />
-        <Row rowID = '4' title = 'TopRated' fetchURL = {requests.requestTopRated} />
+        <Row rowID = '1' title = 'Lançamentos' fetchURL = {requests.requestUpcoming} />
+        <Row rowID = '2' title = 'Populares' fetchURL = {requests.requestPopular} />
+        <Row rowID = '3' title = 'Em alta' fetchURL = {requests.requestTrending} />
+        <Row rowID = '4' title = 'Aclamados pela crítica' fetchURL = {requests.requestTopRated} />
         <Row rowID = '5' title = 'Horror' fetchURL = {requests.requestHorror} />
     </div>
   )
